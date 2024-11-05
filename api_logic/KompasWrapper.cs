f2ead6d0e98398aa48e36e21fae9bdaf3dcbf47f
glotov.daniil <mptts28@gmail.com>
2024-11-05 16:35:55 +0700

    the top building is done

--- api_logic/KompasWrapper.cs
+++ api_logic/KompasWrapper.cs
@@ -46,7 +46,6 @@ namespace api_logic
             sketch.Name = "Эскиз: столешница";
             sketch.Hidden = false;
             sketch.Angle = 90.0;
-            //sketch.LeftHandedCS = true;
             sketch.Update();
 
             IKompasDocument documentSketch = sketch.BeginEdit();
@@ -58,15 +57,33 @@ namespace api_logic
             IRectangle rectangle = drawingContainer.Rectangles.Add();
             rectangle.Style = (int)Kompas6Constants.ksCurveStyleEnum.ksCSNormal;
 
-            Parameter topHeight;
+            Parameter topWidth;
             Parameter topDepth;
+            Parameter topHeight;
+            parameters.Params.TryGetValue(ParamType.TopWidth, out topWidth);
+            parameters.Params.TryGetValue(ParamType.TopDepth, out topDepth);
             parameters.Params.TryGetValue(ParamType.TopHeight, out topHeight);
-            parameters.Params.TryGetValue(ParamType.TopHeight, out topDepth);
 
-            rectangle.Width = topHeight.Value;
+            rectangle.Width = topWidth.Value;
             rectangle.Height = topDepth.Value;
             rectangle.Update();
             sketch.EndEdit();
+
+            //объект модели "Операция выдавливания"
+            IExtrusions extrusions = modelContainer.Extrusions;
+            IExtrusion extrusion = extrusions.Add(Kompas6Constants3D.ksObj3dTypeEnum.o3d_bossExtrusion);
+            extrusion.Direction = Kompas6Constants3D.ksDirectionTypeEnum.dtNormal;
+            extrusion.Name = "Элемент выдавливания: столешница";
+            extrusion.Hidden = false;
+            extrusion.ExtrusionType[true] = Kompas6Constants3D.ksEndTypeEnum.etBlind;
+            extrusion.DraftOutward[true] = false;
+            extrusion.DraftValue[true] = 0.0;
+            extrusion.Depth[true] = topHeight.Value;
+
+            IExtrusion1 extrusion1 = (IExtrusion1)extrusion;
+            extrusion1.Profile = sketch;// modelContainer.Objects[Kompas6Constants3D.ksObj3dTypeEnum.o3d_sketch][0];
+            extrusion1.DirectionObject = sketch;// modelContainer.Objects[Kompas6Constants3D.ksObj3dTypeEnum.o3d_sketch][0];
+            extrusion.Update();
         }
 
         public void OpenCad()
