28d98b4e6fd601ef8a9ec3b8b85f0cd2bc6cea74
isaacparson <isaacparson28@gmail.com>
2025-01-11 19:58:51 +0700

    Валидируем значение относительно минимального и максимального в параметре

--- logic/Parameter.cs
+++ logic/Parameter.cs
@@ -1,32 +1,42 @@
-﻿namespace ParametersLogic
+﻿using System.CodeDom;
+
+namespace ParametersLogic
 {
     /// <summary>
     /// Параметр, используемый для передачи размеров при построении модели в САПР
     /// </summary>
     public class Parameter
     {
+        int _value;
         /// <summary>
         /// ctor.
         /// </summary>
         /// <param name="value">Числовое значение параметра</param>
-        public Parameter(int value)
+        public Parameter(int value, int minValue, int maxValue)
         {
-            Value = value;
+            _value = value;
+            if (value < minValue || value > maxValue)
+            {
+                throw new System.Exception();
+            }
         }
 
         /// <summary>
         /// Максимальное значение параметра.
         /// </summary>
-        public int MaxValue { get; set; }
+        public int MaxValue { get; }
 
         /// <summary>
         /// Минимальное значение параметра.
         /// </summary>
-        public int MinValue { get; set; }
+        public int MinValue { get; }
 
         /// <summary>
         /// Значение параметра.
         /// </summary>
-        public int Value { get; set; }
+        public int Value 
+        { 
+            get => _value;
+        }
     }
 }
